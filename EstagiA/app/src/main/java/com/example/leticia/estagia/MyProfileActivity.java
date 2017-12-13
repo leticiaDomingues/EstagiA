cb8e8ebeb59bb5913498cefbd7568413bc11050f
egrojCarvalho <egrojcarvalho@hotmail.com>
2017-12-13 20:03:14 -0200

    Pega imagem da galeria e adiciona no perfil. Precisa adicionar no user.

--- EstagiA/app/src/main/java/com/example/leticia/estagia/MyProfileActivity.java
+++ EstagiA/app/src/main/java/com/example/leticia/estagia/MyProfileActivity.java
@@ -1,22 +1,37 @@
 package com.example.leticia.estagia;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.RadioButton;
+import android.widget.TextView;
+
 
 public class MyProfileActivity extends Activity implements View.OnClickListener {
+    private static final int SELECT_PICTURE = 1;
+    Uri selectedImagePath;
+    private ImageView hImagePerfil;
+    private Button hButton;
+
+    @SuppressLint("ResourceType")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_profile);
 
-        if(MyDB.ra.equals("")) {
+        if (MyDB.ra.equals("")) {
             //display the main activity
             startActivity(new Intent(this, MainActivity.class));
         } else {
@@ -31,14 +46,22 @@ public class MyProfileActivity extends Activity implements View.OnClickListener 
             txtEmail.setText(user.getEmail());
             EditText txtSenha = findViewById(R.id.txtSenha);
             txtSenha.setText(user.getSenha());
-            RadioButton radioBcc = (RadioButton)findViewById(R.id.radio_bcc);
-            RadioButton radioEnc = (RadioButton)findViewById(R.id.radio_enc);
-            if(user.getCurso().equals("BCC")) {
+            RadioButton radioBcc = (RadioButton) findViewById(R.id.radio_bcc);
+            RadioButton radioEnc = (RadioButton) findViewById(R.id.radio_enc);
+            if (user.getCurso().equals("BCC")) {
                 radioBcc.setChecked(true);
             } else {
                 radioEnc.setChecked(true);
             }
         }
+        (findViewById(R.id.Galeria))
+                .setOnClickListener(new View.OnClickListener() {
+
+                    public void onClick(View arg0) {
+                            Intent i = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                            startActivityForResult(i, 1);
+                    }
+                });
 
     }
 
@@ -68,18 +91,36 @@ public class MyProfileActivity extends Activity implements View.OnClickListener 
         }
     }
 
+    public void onClick(View v) {
+
+    }
+
+@Override
+protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == 1 && resultCode == RESULT_OK && null != data) {
+        Uri selectedImage = data.getData();
+        String[] filePathColumn = { MediaStore.Images.Media.DATA };
+        Cursor cursor = getContentResolver().query(selectedImage,filePathColumn, null, null, null);
+        cursor.moveToFirst();
+        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+        String picturePath = cursor.getString(columnIndex);
+        cursor.close();
+        ImageView imageView = findViewById(R.id.imgPerfil);
+        imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+    }
+}
+
     @Override
     protected void onStart() {
         super.onStart();
 
         //if the user is not logged in
-        if(MyDB.ra.equals("")) {
+        if (MyDB.ra.equals("")) {
             //display the main activity
             startActivity(new Intent(this, MainActivity.class));
         }
     }
-
-    public void onClick(View v) {
-
-    }
 }
+
+
